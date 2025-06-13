7cc77b2877c0ac98309128916a47e9827735122f
Bartek <baarti02@gmail.com>
2025-06-13 13:23:02 +0200

    Created new endpoints nad refactored previous

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,113 +1,191 @@
 package com.example
 
 import com.example.model.*
+import com.example.repository.ProductRepository
+import com.example.repository.UserRepository
+import com.example.services.OpenFoodFactsService
+import com.example.utils.IngredientTagHelper
+
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
 fun Application.configureRouting() {
+    val productRepository = ProductRepository()
+    val userRepository = UserRepository()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+            call.principal<UserIdPrincipal>()?.name
         }
 
-        route("/product") {
-            get("/{barcode}") {
-                val barcode = call.parameters["barcode"]
+        authenticate {
+            route("/product") {
+                get("/{barcode}") {
+                    val barcode = call.parameters["barcode"]
 
-                if (barcode == null) {
-                    call.respondText("Brakuje barcode", status = HttpStatusCode.BadRequest)
-                    return@get
-                }
-
-                val productWithIngredients = transaction {
-                    val productRow = Products.selectAll().where { Products.barcode eq barcode }.singleOrNull()
-                    if (productRow == null) {
-                        return@transaction null
+                    if (barcode == null) {
+                        call.respondText("Brakuje barcode",
+                            status = HttpStatusCode.BadRequest)
+                        return@get
                     }
 
-                    val productId = productRow[Products.id]
+                    val principal = call.principal<UserIdPrincipal>()
+                    val uid = principal?.name
+                    println("Zalogowany UID: $uid")
 
-                    val ingredients = IngredientsTable
-                        .selectAll()
-                        .where { IngredientsTable.product eq productId }
-                        .map {
-                            Ingredient(
-                                name = it[IngredientsTable.name],
-                                tag = it[IngredientsTable.tag]
-                            )
-                        }
-
-                    ProductResponse(
-                        name = productRow[Products.name],
-                        macros = Macros(
-                            calories = productRow[Products.calories],
-                            protein = productRow[Products.protein],
-                            fat = productRow[Products.fat],
-                            carbs = productRow[Products.carbs],
-                        ),
-                        ingredients = ingredients,
-                    )
-                }
-
-                if (productWithIngredients == null) {
-                    call.respond(HttpStatusCode.NotFound)
-                } else {
-                    call.respond(productWithIngredients)
-                }
-            }
-
-            post("/save") {
-                val product = call.receive<ProductRequest>()
-                transaction {
-                    val productId = Products.insertAndGetId {
-                        it[barcode] = product.barcode
-                        it[name] = product.name
-                        it[calories] = product.macrosPer100g.calories
-                        it[protein] = product.macrosPer100g.protein
-                        it[fat] = product.macrosPer100g.fat
-                        it[carbs] = product.macrosPer100g.carbs
-                        it[weight] = product.grams
-                    }
-
-                    product.ingredients.forEach { ingredientName ->
-                        IngredientsTable.insert {
-                            it[IngredientsTable.product] = productId
-                            it[name] = ingredientName
-                            it[tag] = getTagForIngredient(ingredientName)
+                    val product = productRepository.getProductFromDatabase(barcode)
+                    if (product != null) {
+                        call.respond(product)
+                    } else {
+                        val fetched = OpenFoodFactsService.fetchProduct(barcode)
+                        if (fetched != null) {
+                            call.respond(fetched)
+                        } else {
+                            call.respond(HttpStatusCode.NotFound, "Nie znaleziono produktu.")
                         }
                     }
                 }
-                call.respondText("Produkt i składniki zapisane")
-            }
 
-            get("/all") {
-                val products = transaction {
-                    Products.selectAll().map {
-                        ProductDTO(
-                            barcode = it[Products.barcode],
-                            name = it[Products.name],
-                            calories = it[Products.calories],
-                            protein = it[Products.protein],
-                            fat = it[Products.fat],
-                            carbs = it[Products.carbs],
-                            weight = it[Products.weight]
-                        )
+                delete("/{barcode}") {
+                    val currentUserUID = call.principal<UserIdPrincipal>()?.name
+                    if (currentUserUID == null) {
+                        call.respond(HttpStatusCode.Unauthorized, "Brak autoryzacji")
+                        return@delete
+                    }
+
+                    val barcode = call.parameters["barcode"]
+                    if (barcode == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Brakuje barcode")
+                        return@delete
+                    }
+
+                    val user = userRepository.findByUid(currentUserUID)
+                    if (user == null) {
+                        call.respond(HttpStatusCode.NotFound, "Użytkownik nie istnieje")
+                        return@delete
+                    }
+
+                    val deleted = transaction {
+                        val product = Products
+                            .selectAll()
+                            .where { Products.barcode eq barcode }
+                            .andWhere { Products.user eq user.id }
+                            .singleOrNull()
+
+                        if (product == null) {
+                            false
+                        } else {
+                            val productId = product[Products.id]
+                            IngredientsTable
+                                .deleteWhere { IngredientsTable.product eq productId }
+                            Products
+                                .deleteWhere { Products.id eq productId }
+                            true
+                        }
+                    }
+
+                    if (deleted) {
+                        call.respond(HttpStatusCode.OK, "Produkt został usunięty")
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, "Nie znaleziono produktu użytkownika")
                     }
                 }
-                call.respond(products)
+
+                post("/save") {
+                    val product = call.receive<ProductRequest>()
+
+                    val currentUserUID = call.principal<UserIdPrincipal>()?.name
+                    if (currentUserUID == null) {
+                        call.respond(HttpStatusCode.Unauthorized, "Brak autoryzacji")
+                        return@post
+                    }
+
+                    val user = userRepository.findByUid(currentUserUID)
+                    if (user == null) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@post
+                    }
+
+                    val alreadyExists = transaction {
+                        val p = Products
+                            .selectAll()
+                            .where { Products.barcode eq product.barcode }
+                            .andWhere { Products.user eq user.id }
+                            .singleOrNull()
+
+                        p != null
+                    }
+
+                    if (alreadyExists) {
+                        call.respond(HttpStatusCode.BadRequest,
+                            "Produkt o tym kodzie kreskowym już istnieje")
+                        return@post
+                    }
+
+                    transaction {
+                        val productId = Products.insertAndGetId {
+                            it[barcode] = product.barcode
+                            it[name] = product.name
+                            it[calories] = productRepository.calculateMacroForProduct(product.macrosPer100g.calories, product.grams)
+                            it[protein] = productRepository.calculateMacroForProduct(product.macrosPer100g.protein, product.grams)
+                            it[fat] = productRepository.calculateMacroForProduct(product.macrosPer100g.fat, product.grams)
+                            it[carbs] = productRepository.calculateMacroForProduct(product.macrosPer100g.carbs, product.grams)
+                            it[weight] = product.grams
+                            it[Products.user] = user.id
+                        }
+
+                        product.ingredients.forEach { ingredientName ->
+                            IngredientsTable.insert {
+                                it[IngredientsTable.product] = productId
+                                it[name] = ingredientName
+                                it[tag] = IngredientTagHelper.getIngredientTag(ingredientName)
+                            }
+                        }
+                    }
+                    call.respondText("Produkt i składniki zapisane")
+                }
+
+
+                get("/all") {
+                    val currentUserUID = call.principal<UserIdPrincipal>()?.name
+                    if (currentUserUID == null) {
+                        call.respond(HttpStatusCode.Unauthorized, "Brak autoryzacji")
+                        return@get
+                    }
+
+                    val findUser = userRepository.findByUid(currentUserUID)
+                    if (findUser == null) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+
+                    val products = transaction {
+                        Products.selectAll().where { Products.user eq findUser.id }
+                            .map {
+                                ProductDTO(
+                                    barcode = it[Products.barcode],
+                                    name = it[Products.name],
+                                    calories = it[Products.calories],
+                                    protein = it[Products.protein],
+                                    fat = it[Products.fat],
+                                    carbs = it[Products.carbs],
+                                    weight = it[Products.weight]
+                                )
+                            }
+                    }
+                    call.respond(products)
+                }
             }
+
         }
     }
-}
-
-fun getTagForIngredient(ingredientName: String): String {
-    return "neutral"
 }
